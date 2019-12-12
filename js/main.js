b664b6333cee6bc76e58fe0c175f9d03c6fbbcfa
MuttakinHasib <hasibmolla28@gmail.com>
2019-12-12 18:29:00 +0600

    added curriculum section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Navbar Active
 $(window).scroll(function() {
     var winScroll = $(this).scrollTop();
     if (winScroll >= 110) {
@@ -6,3 +7,39 @@ $(window).scroll(function() {
         $("nav").removeClass("header-active");
     }
 });
+
+$(document).ready(function() {
+    //Animate Click Scroll
+
+    var scrollLink = $(".scroll");
+
+    scrollLink.click(function(e) {
+        e.preventDefault();
+
+        $("body,html").animate(
+            {
+                scrollTop: $(this.hash).offset().top - 100
+            },
+            1000
+        );
+    });
+});
+
+// Tab Panel
+const tabLink = document.querySelectorAll(".tab-link");
+const tabContent = document.querySelectorAll(".tab-content");
+
+function showPanel(index, color) {
+    tabLink.forEach(item => {
+        item.style.color = "";
+        item.style.borderBottomColor = "";
+    });
+    tabContent.forEach(item => {
+        item.style.display = "none";
+    });
+    tabLink[index].style.color = color;
+    tabLink[index].style.borderBottomColor = color;
+    tabContent[index].style.display = "block";
+}
+
+showPanel(0,'#f36');
