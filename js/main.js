e1ba275b0984e4a2a73a4a05040c5c5baa129526
MuttakinHasib <hasibmolla28@gmail.com>
2019-12-25 00:44:00 +0600

    added admission section

--- js/main.js
+++ js/main.js
@@ -1,59 +1,87 @@
 // Navbar Active
 $(window).scroll(function() {
-    var winScroll = $(this).scrollTop();
-    if (winScroll >= 110) {
-        $("nav").addClass("header-active");
-    } else {
-        $("nav").removeClass("header-active");
-    }
+  var winScroll = $(this).scrollTop();
+  if (winScroll >= 110) {
+    $("nav").addClass("header-active");
+  } else {
+    $("nav").removeClass("header-active");
+  }
 });
 
 $(document).ready(function() {
-    //Animate Click Scroll
+  //Animate Click Scroll
 
-    var scrollLink = $(".scroll");
+  var scrollLink = $(".scroll");
 
-    scrollLink.click(function(e) {
-        e.preventDefault();
+  scrollLink.click(function(e) {
+    e.preventDefault();
 
-        $("body,html").animate(
-            {
-                scrollTop: $(this.hash).offset().top - 100
-            },
-            1000
-        );
-    });
+    $("body,html").animate(
+      {
+        scrollTop: $(this.hash).offset().top - 100
+      },
+      1000
+    );
+  });
+  $(".brand-img").owlCarousel({
+    items: 1,
+    loop: true,
+    // dots: true,
+    // nav: true,
+    autoplay: true,
+    mouseDrag: true,
+    responsive: {
+      1280: {
+        items: 5
+      },
+      900: {
+        items: 3
+      },
+      600: {
+        items: 2
+      },
+      400: {
+        items: 2
+      }
+    }
+  });
 });
 
 // Tab Panel
-const curriculumTabLink = document.querySelectorAll(".curriculum-tab .tab-link");
-const curriculumTabContent = document.querySelectorAll(".curriculum-content .tab-content");
+const curriculumTabLink = document.querySelectorAll(
+  ".curriculum-tab .tab-link"
+);
+const curriculumTabContent = document.querySelectorAll(
+  ".curriculum-content .tab-content"
+);
 const toolsTabLink = document.querySelectorAll(".tools-tab .tab-link");
-const toolsTabContent = document.querySelectorAll(".tools-content .tab-content");
+const toolsTabContent = document.querySelectorAll(
+  ".tools-content .tab-content"
+);
 function showPanel(index, color) {
-    curriculumTabLink.forEach(item => {
-        item.style.color = "";
-        item.style.borderBottomColor = "";
-    });
-    curriculumTabContent.forEach(item => {
-        item.style.display = "none";
-    });
-    curriculumTabLink[index].style.color = color;
-    curriculumTabLink[index].style.borderBottomColor = color;
-    curriculumTabContent[index].style.display = "block";
+  curriculumTabLink.forEach(item => {
+    item.style.color = "";
+    item.style.borderBottomColor = "";
+  });
+  curriculumTabContent.forEach(item => {
+    item.style.display = "none";
+  });
+  curriculumTabLink[index].style.color = color;
+  curriculumTabLink[index].style.borderBottomColor = color;
+  curriculumTabContent[index].style.display = "block";
 }
 
-function showToolsPanel(index,color){
-    toolsTabLink.forEach(item => {
-        item.style.color = "";
-        item.style.borderBottomColor = "";
-    });
-    toolsTabContent.forEach(item => {
-        item.style.display = "none";
-    });
-    toolsTabLink[index].style.color = color;
-    toolsTabLink[index].style.borderBottomColor = color;
-    toolsTabContent[index].style.display = "block";
+function showToolsPanel(index, color) {
+  toolsTabLink.forEach(item => {
+    item.style.color = "";
+    item.style.borderBottomColor = "";
+  });
+  toolsTabContent.forEach(item => {
+    item.style.display = "none";
+  });
+  toolsTabLink[index].style.color = color;
+  toolsTabLink[index].style.borderBottomColor = color;
+  toolsTabContent[index].style.display = "block";
 }
-showPanel(0,'#f36');
-showToolsPanel(0,'#f36');
+showPanel(0, "#f36");
+showToolsPanel(0, "#f36");
